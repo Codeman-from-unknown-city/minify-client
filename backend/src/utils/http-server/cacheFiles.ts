3cf1479d2c6f6c046160dd0e0c51966c4e4ccf1c
Sergei Truschev <i_truschev@mail.ru>
2020-08-04 18:48:39 +0900

    delete extra

--- backend/src/utils/http-server/cacheFiles.ts
+++ backend/src/utils/http-server/cacheFiles.ts
@@ -2,13 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 class Cash extends Map {
-    addFile(filePath: string, fileName: string, dirName: string): void {
+    addFile(filePath: string): void {
         const ext: string = path.extname(filePath).substring(1);
-        let fileContent: string = '\0';
-        const callback = (err: Error, result: string): void => {
+        const callback = (err: Error, fileContent: string): void => {
             if (err) throw err;
 
-            fileContent = result;
+            super.set(filePath, fileContent);
         }
     
         switch(ext) {
@@ -20,25 +19,19 @@ class Cash extends Map {
             default:
                 fs.readFile(filePath, 'utf8', callback); 
         }
-    
-        super.get(dirName)[fileName] = fileContent;
     }
 
-    addDirectory(directoryPath: string, isNested: boolean = false): void {
+    addDirectory(directoryPath: string): void {
         fs.readdir(directoryPath,
         {encoding: null, withFileTypes: true}, 
         (err: Error, files: any) => {
             if (err) throw err;
-            
-            const propName = directoryPath.split('/')[directoryPath.length - 1];
-            if (!isNested) super.set(propName, {});
 
             for (let file of files) {
-                const fileName = file.name;
-                const filePath: string = path.join(directoryPath, fileName);
+                const filePath: string = path.join(directoryPath, file.name);
 
-                if (file.isDirectory()) this.addDirectory(filePath, true);
-                else this.addFile(filePath, fileName, propName);
+                if (file.isDirectory()) this.addDirectory(filePath);
+                else this.addFile(filePath);
             }
         });
     }
