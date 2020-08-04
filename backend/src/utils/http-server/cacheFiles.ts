e9d64a20dd70fdb65bec8ec601fc0130f3e3f7f8
Sergei Truschev <i_truschev@mail.ru>
2020-08-04 17:52:21 +0900

    add cash logic

---
+++ backend/src/utils/http-server/cacheFiles.ts
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
+class Cash extends Map {
+    addFile(filePath: string): string {
+        const ext: string = path.extname(filePath).substring(1);
+        let fileContent: string = '\0';
+        const callback = (err: Error, result: string): void => {
+            if (err) throw err;
+
+            fileContent = result;
+        }
+    
+        switch(ext) {
+            case 'png':
+            case 'jpg':
+                fs.readFile(path, callback);
+                break;
+    
+            default:
+                fs.readFile(path, 'utf8', callback); 
+        }
+    
+        return fileContent;
+    }
+
+    addDirectory(directoryPath: string, name: string, isNested: boolean = false): void {
+        fs.readdir(directoryPath,
+        {encoding: null, withFileTypes: true}, 
+        (err: Error, files: any) => {
+            if (err) throw err;
+            
+            if (!isNested) super.set(name, {});
+
+            for (let file of files) {
+                const filePath: string = path.join(directoryPath, file.name);
+
+                if (file.isDirectory()) this.addDirectory(filePath, name, true);
+
+                const fileContent = this.addFile(filePath);
+                super.get(name)[file.name] = fileContent;
+            }
+        });
+    }
+}
+
+const CASH = new Cash();
+
+export {CASH};
