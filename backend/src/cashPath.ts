b4e87a8802c3e359683eebf74c32c8093cadd5d4
Sergei Truschev <i_truschev@mail.ru>
2020-08-05 16:31:49 +0900

    returned to the previous version

--- backend/src/cashPath.ts
+++
@@ -1,3 +0,0 @@
-const joinPath = require('path').join;
-
-export const cashPath = joinPath(process.cwd(), '/cash');
--- backend/src/utils/http-server/cacheFiles.ts
+++ backend/src/utils/http-server/cacheFiles.ts
@@ -1,58 +1,43 @@
-/// <reference path="./../interfaces.ts" />
-
-import { cashPath } from "../../cashPath"
-
-const fsPromises = require('fs').promises;
-const v8 = require('v8');
+const fs = require('fs');
 const path = require('path');
 
+class Cash extends Map {
+    addFile(filePath: string): void {
+        const ext: string = path.extname(filePath).substring(1);
+        const callback = (err: Error, fileContent: string): void => {
+            if (err) throw err;
 
-const cashAPI: I.Cash = {
-    writeCash(cash: any) {
-        const cashBuffer: Buffer = v8.serialize(cash ? cash : new Map());
-
-        return fsPromises.writeFile(cashPath, cashBuffer, {encoding: null});
-    },
-
-    async getCash() {
-        const cashBuffer: Buffer = await fsPromises.readFile(cashPath);
-
-        return v8.deserialize(cashBuffer);
-    },
+            super.set(filePath, fileContent);
+        }
     
-    addDir(dirPath: string) {
-        fsPromises.readdir(dirPath,
+        switch(ext) {
+            case 'html':
+            case 'css':
+            case 'js':
+                fs.readFile(filePath, 'utf8', callback); 
+                break;
+    
+            default:
+                fs.readFile(filePath, callback);
+        }
+    }
+
+    addDirectory(directoryPath: string): void {
+        fs.readdir(directoryPath,
         {encoding: null, withFileTypes: true}, 
         (err: Error, files: any) => {
             if (err) throw err;
 
             for (let file of files) {
-                const filePath: string = path.join(dirPath, file.name);
-                if (file.isDirectory()) this.addDir(filePath);
+                const filePath: string = path.join(directoryPath, file.name);
+
+                if (file.isDirectory()) this.addDirectory(filePath);
                 else this.addFile(filePath);
             }
         });
-    },
-    
-    async addFile(filePath: string) {
-        const cash: any = this.getCash();
-        const ext: string = path.extname(filePath).substring(1);
-        let fileContent: string;
+    }
+}
 
-        switch (ext) {
-            case 'js':
-            case 'html':
-            case 'css':
-                fileContent = await fsPromises.readFile(filePath, 'utf8');
-                break;
+const cash = new Cash();
 
-            default:
-                fileContent = await fsPromises.readFile(filePath, {encoding: null});
-        }
-
-        cash.set(filePath, fileContent);
-        this.writeCash(cash);
-    },
-};
-
-export {cashAPI};
+export {cash};
