7debdb56188bb2364615ec2e64261417f61f76ac
Sergei Truschev <i_truschev@mail.ru>
2020-08-05 19:31:31 +0900

    add mime types

---
+++ src/utils/http-server/mimeTypes.ts
@@ -0,0 +1,7 @@
+export const MIME_TYPES: { [index: string]: string } = {
+    'html': 'text/html',
+    'css': 'text/css',
+    'js': 'application/javascript',
+    'png': 'image/png',
+    'jpg': 'image/jpeg',
+}
