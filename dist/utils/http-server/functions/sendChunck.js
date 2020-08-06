d2ad0de403e86f49f66e597ad8a30defa2acc75a
Sergei Truschev <i_truschev@mail.ru>
2020-08-06 15:04:25 +0900

    added minification to given fiels

--- dist/utils/http-server/functions/sendChunck.js
+++ dist/utils/http-server/functions/sendChunck.js
@@ -1,14 +1,19 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = require("path");
 const mimeTypes_1 = require("./../objects/mimeTypes");
+const minifyCode_1 = __importDefault(require("../../minifyCode"));
 function sendChunck(res, path, chunck) {
     const ext = path_1.extname(path).substring(1);
+    const compressedChunk = minifyCode_1.default({ code: chunck.toString(), ext, name: path_1.basename(path) });
     res
         .writeHead(200, 'ok', {
-        'Content-Length': chunck.length,
+        'Content-Length': compressedChunk.length,
         'Content-type': mimeTypes_1.MIME_TYPES[ext] || mimeTypes_1.MIME_TYPES['txt'],
     })
-        .end(chunck);
+        .end(compressedChunk);
 }
 exports.default = sendChunck;
--- src/utils/http-server/functions/sendChunck.ts
+++ src/utils/http-server/functions/sendChunck.ts
@@ -1,14 +1,16 @@
-import { extname } from "path";
+import { extname, basename } from "path";
 import { MIME_TYPES } from "./../objects/mimeTypes";
 import { ServerResponse } from "http";
+import minify from "../../minifyCode";
 
 export default function sendChunck(res: ServerResponse, path: string, chunck: string | Buffer): void {
     const ext: string = extname(path).substring(1);
+    const compressedChunk = minify( {code: chunck.toString(), ext, name: basename(path)} );
 
     res
       .writeHead(200, 'ok', {
-          'Content-Length': chunck.length,
+          'Content-Length': compressedChunk.length,
           'Content-type': MIME_TYPES[ext] || MIME_TYPES['txt'],
       })
-      .end(chunck);
+      .end(compressedChunk);
 }
