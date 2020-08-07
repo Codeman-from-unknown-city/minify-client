cac366b94cecdaf750ee187ab52763337fdd914a
Sergei Truschev <i_truschev@mail.ru>
2020-08-08 04:05:15 +0900

    add errors sender

--- dist/utils/http-server/functions/requestHandlers.js
+++ dist/utils/http-server/functions/requestHandlers.js
@@ -7,31 +7,42 @@ exports.handlePost = exports.handleGet = void 0;
 const cash_1 = require("../../cash");
 const sendChunck_1 = __importDefault(require("./sendChunck"));
 const path_1 = require("path");
+const notBindedSendError = (res, code, reason = '', chunk = '') => res.writeHead(code, reason).end(chunk);
 function handleGet(res, url) {
-    if (url) {
-        const STATIC_PATH = path_1.join(process.cwd(), 'static');
-        if (url === '/') {
-            const indexPath = path_1.join(STATIC_PATH, 'index.html');
-            const index = cash_1.cash.get(indexPath);
-            if (index) {
-                sendChunck_1.default(res, indexPath, index);
-                return;
-            }
-        }
-        const cashedFile = cash_1.cash.get(path_1.join(STATIC_PATH, url));
-        if (cashedFile)
-            sendChunck_1.default(res, url, cashedFile);
+    const sendError = notBindedSendError.bind(null, res);
+    if (!url) {
+        sendError(500, 'Unforessen situation');
         return;
     }
+    const STATIC_PATH = path_1.join(process.cwd(), 'static');
+    if (url === '/') {
+        const indexPath = path_1.join(STATIC_PATH, 'index.html');
+        const index = cash_1.cash.get(indexPath);
+        if (!index) {
+            sendError(500, 'Page prepare cash');
+            return;
+        }
+        sendChunck_1.default(res, indexPath, index);
+        return;
+    }
+    const cashedFile = cash_1.cash.get(path_1.join(STATIC_PATH, url));
+    if (cashedFile)
+        sendChunck_1.default(res, url, cashedFile);
 }
 exports.handleGet = handleGet;
 function handlePost(req, res) {
+    const sendError = notBindedSendError.bind(null, res);
     let body = '';
     req.on('data', (chunk) => {
         body += chunk.toString();
     });
     req.on('end', () => {
-        res.end();
+        const ip = req.socket.remoteAddress;
+        if (!ip) {
+            sendError(500, 'Unforessen situation');
+            return;
+        }
+        const userId = ip.split('').reduce((sum, current) => +sum + +current, 0);
     });
 }
 exports.handlePost = handlePost;
--- src/utils/http-server/functions/requestHandlers.ts
+++ src/utils/http-server/functions/requestHandlers.ts
@@ -3,27 +3,41 @@ import { cash } from "../../cash";
 import sendChunck from "./sendChunck";
 import { join } from "path";
 
+const notBindedSendError = (
+    res: ServerResponse, 
+    code: number, 
+    reason: string = '', 
+    chunk: Buffer | string = ''
+    ): void => res.writeHead(code, reason).end(chunk); 
+
 function handleGet(res: ServerResponse, url: string | undefined): void {
-    if (url) {
-        const STATIC_PATH: string = join(process.cwd(), 'static');
+    const sendError = notBindedSendError.bind(null, res);
 
-        if (url === '/') {
-            const indexPath: string = join(STATIC_PATH, 'index.html');
-            const index: string | Buffer = cash.get(indexPath);
-            if (index) {
-                sendChunck(res, indexPath, index);
-                return;
-            }   
-        }
-        
-        const cashedFile: string | Buffer | undefined = cash.get( join(STATIC_PATH, url) );
-        if (cashedFile) sendChunck(res, url, cashedFile);
-
+    if (!url) {
+        sendError(500, 'Unforessen situation');
         return;
     }
+      
+    const STATIC_PATH: string = join(process.cwd(), 'static');
+
+    if (url === '/') {
+        const indexPath: string = join(STATIC_PATH, 'index.html');
+        const index: string | Buffer | undefined = cash.get(indexPath);
+        if (!index) {
+            sendError(500, 'Page prepare cash');
+            return;
+        }
+        
+        sendChunck(res, indexPath, index);
+        return;
+    }
+        
+    const cashedFile: string | Buffer | undefined = cash.get( join(STATIC_PATH, url) );
+    if (cashedFile) sendChunck(res, url, cashedFile);
 }
 
 function handlePost(req: IncomingMessage, res: ServerResponse): void {
+    const sendError = notBindedSendError.bind(null, res);
     let body: string = '';
 
     req.on('data', (chunk: Buffer): void => {
@@ -31,8 +45,13 @@ function handlePost(req: IncomingMessage, res: ServerResponse): void {
     });
 
     req.on('end', (): void => {
+        const ip: string | undefined = req.socket.remoteAddress;
+        if (!ip) {
+            sendError(500, 'Unforessen situation');
+            return;
+        }
 
-        res.end();
+        const userId: number = ip.split('').reduce((sum, current) => +sum + +current, 0);
     });
 }
 
